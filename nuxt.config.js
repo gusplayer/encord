4c20e37a453d9226d34160ee10efc410bb71175e
Gustavo Moreno <gustavo@fosterapps.com>
2018-08-22 21:57:40 -0500

    sds

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#3B8470' },
   mode: 'spa',
   /*
   ** Build configuration
