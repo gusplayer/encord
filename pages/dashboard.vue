8f90635cd80b61325d2d46da7d5624ff4a1ae805
DESKTOP-VKKL5LV\usuario <titanmar15@gmail.com>
2018-09-06 17:56:21 -0500

    listo dashboard para agregar componentes de contenido

---
+++ pages/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard">
+    <sidebar/>
+    <nuxt-child class="dashboard-view" />
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/sidebar'
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style scoped>
+.dashboard {
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
+</style>
